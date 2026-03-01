c670043983794ecb0061765244607a73f9403aa6
artfourteen <shadiyarovarmat@gmail.com>
2026-03-01 13:24:59 +0500

    feat: in memory cache

---
+++ cache.go
@@ -0,0 +1,27 @@
+package cache
+
+type ICache interface {
+	Set(key string, value interface{})
+	Get(key string) interface{}
+	Delete(key string)
+}
+
+type Cache struct {
+	memory map[string]interface{}
+}
+
+func (c *Cache) Set(key string, value interface{}) {
+	c.memory[key] = value
+}
+
+func (c *Cache) Get(key string) interface{} {
+	return c.memory[key]
+}
+
+func (c *Cache) Delete(key string) {
+	delete(c.memory, key)
+}
+
+func New() Cache {
+	return Cache{memory: make(map[string]interface{})}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/artfourteen/in-memory-cache
+
+go 1.25.7
